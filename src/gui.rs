7c4a051e4b2f5cadeeeff807c797e68481de3dc6
ERROR_PC <yoyosameh310@gmail.com>
2022-11-11 21:30:40 +0200

    Updated to iced 0.5

--- src/gui.rs
+++ src/gui.rs
@@ -1,24 +1,52 @@
-use iced::{Sandbox, Button, Text};
+use iced::{
+    Sandbox, Alignment, Theme
+};
+
+use iced::widget::{
+    button, column, text
+};
+
+#[derive(Debug, Clone, Copy)]
+pub enum Pressed {
+    Plus,
+    Minus,
+}
 
 #[derive(Debug, Clone, Copy, Default)]
-pub struct Calculator;
+pub struct Calculator {
+    value: f64,
+}
 
 impl Sandbox for Calculator {
-    type Message = ();
+    type Message = Pressed;
 
     fn new() -> Self {
-        Self
+        Self::default()
     }
 
     fn title(&self) -> String {
-        String::from("A test application!")
+        String::from("The le epic calculator")
     }
 
-    fn view(&mut self) -> iced::Element<'_, Self::Message> {
-        Text::new("Hello world!").into()
+    fn update(&mut self, message: Self::Message) {
+        match message {
+            Pressed::Plus => self.value += 1.0,
+            Pressed::Minus => self.value -= 1.0,
+        }
     }
 
-    fn update(&mut self, _message: Self::Message) {
-        
+    fn view(&self) -> iced::Element<'_, Self::Message> {
+        column![
+            button("+").on_press(Pressed::Plus),
+            text(self.value),
+            button("-").on_press(Pressed::Minus),
+        ]
+            .padding(20)
+            .align_items(Alignment::Fill)
+            .into()
+    }
+
+    fn theme(&self) -> Theme {
+        Theme::Dark
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 mod gui;
 
-use iced::{Sandbox, Settings};
+use iced::{Sandbox, Settings, window};
 use gui::Calculator;
 
 fn main() -> iced::Result {
-    Calculator::run(Settings::default())
+    Calculator::run(
+        Settings {
+            window: window::Settings { size: (360, 640), ..Default::default() },
+            text_multithreading: true,
+            antialiasing: false,
+            ..Default::default()
+        }
+    )
 }
