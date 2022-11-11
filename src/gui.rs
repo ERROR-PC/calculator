d7f179d4d4e92826122e53240c75b103ac608f44
ERROR_PC <yoyosameh310@gmail.com>
2022-11-11 16:03:11 +0200

    Updating to 0.5

--- src/gui.rs
+++ src/gui.rs
@@ -1,5 +1,9 @@
-use iced::{Sandbox, button, Alignment, Color, Column};
-use iced::widget::{Button, Text};
+use iced::{
+    Sandbox
+};
+use iced::widget::{
+    button, column, text, Column
+};
 
 #[derive(Debug, Clone, Copy)]
 pub enum ButtonPressed {
@@ -10,9 +14,6 @@ pub enum ButtonPressed {
 #[derive(Debug, Clone, Copy, Default)]
 pub struct Calculator {
     value: f64,
-
-    plus_button: button::State,
-    minus_button: button::State,
 }
 
 impl Sandbox for Calculator {
@@ -23,39 +24,19 @@ impl Sandbox for Calculator {
     }
 
     fn title(&self) -> String {
-        String::from("The le epic error calculator")
-    }
-
-    fn view(&mut self) -> iced::Element<'_, Self::Message> {
-        Column::new()
-            .align_items(Alignment::Center)
-            .push(
-                Button::new(&mut self.plus_button, Text::new("+"))
-                .on_press(ButtonPressed::Plus)
-            )
-            .push(
-                Text::new(self.value.to_string())
-                .color(Color::WHITE)
-            )
-            .push(
-                Button::new(&mut self.minus_button, Text::new("-"))
-                    .on_press(ButtonPressed::Minus)
-            )
-            .into()
+        String::from("The le epic calculator")
     }
 
     fn update(&mut self, message: Self::Message) {
         match message {
-            ButtonPressed::Plus => {
-                self.value += 1.0;
-            },
-            ButtonPressed::Minus => {
-                self.value -= 1.0;
-            },
+            ButtonPressed::Plus => self.value += 1.0,
+            ButtonPressed::Minus => self.value -= 1.0,
         }
     }
 
-    fn background_color(&self) -> Color {
-        Color::from_rgb8(30, 30, 30)
+    fn view(&self) -> iced::Element<'_, Self::Message> {
+        column![
+            button("+").on_press(ButtonPressed::Plus),
+        ]
     }
 }
